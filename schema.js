7428c7f372881d4ec87c307dd508170baeb6fa50
Tanaka <tsmadeu@icloud.com>
2018-10-04 00:09:16 +0100

    need to add better error logic

--- schema.js
+++ schema.js
@@ -4,16 +4,16 @@ const { SECRET_TOKEN } = process.env
 const stripe = require('stripe')(SECRET_TOKEN)
 
 // this should return an object of products
-const getProducts = () => stripe.products.list({ limit: 100 }, (err, products) => products)
+const getProducts = () => stripe.products.list({ limit: 100 }, (err, products) => !err && products)
 
 //  get product by ID
-const getProduct = id => stripe.products.retrieve(id, (err, product) => product)
+const getProduct = id => stripe.products.retrieve(id, (err, product) => !err && product)
 
 // get all skus
-const getSKUs = () => stripe.skus.list({ limit: 100 }, (err, skus) => skus)
+const getSKUs = () => stripe.skus.list({ limit: 100 }, (err, skus) => !err && skus)
 
 // get skus by product id?
-const getSKU = id => stripe.skus.retrieve(id, (err, sku) => sku)
+const getSKU = id => stripe.skus.retrieve(id, (err, sku) => !err && sku)
 
 const typeDefs = gql`
     type Query {
@@ -28,13 +28,13 @@ const typeDefs = gql`
         object: String
         active: Boolean
         caption: String
-        created: Number
         description: String
         livemode: Boolean
         name: String
         shippable: Boolean
         type: String
-        updated: Number
+        created: String
+        updated: String
     }
 
     type SKU {
